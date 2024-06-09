1036d88ab383e890bff55c18c46a08977d766243
smit-shah-GG <smitshahofficial@gmail.com>
2024-06-09 10:29:26 +0530

    Made Changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::process::{Command, exit};
+
+fn update_commit_push() {
+    let add_command = Command::new("git")
+    .args(["add", "."])
+    .output()
+    .expect("Failed to execute git add command");
+    
+    let commit_command = Command::new("git")
+    .args(["commit", "-m", "Made Changes"])
+    .output()
+    .expect("Failed to execute git commit command");
+    
+    let push_command = Command::new("git")
+    .args(["push", "origin", "master"])
+    .output()
+    .expect("Failed to execute git push command");
+
+    if !add_command.status.success() || !commit_command.status.success() || !push_command.status.success() {
+        eprintln!("Failed to update repository");
+        exit(1);
+    }
+
+    println!("Updated repository");
+}
+
 fn main() {
-    println!("Hello, world!");
+    update_commit_push();
+    println!("Done!");
 }
